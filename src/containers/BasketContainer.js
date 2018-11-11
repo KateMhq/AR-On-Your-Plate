735c8e0c45e20ec1b3c2e62320279167e0506a9c
jamal westfield <jamalwestfield@gmail.com>
2018-11-11 13:33:01 +0000

    added basket components, scaling bug issue fixed =), rotating and scaling fine now

---
+++ src/containers/BasketContainer.js
@@ -0,0 +1,20 @@
+import MenuItem from "../components/Basket";
+import { connect } from "react-redux";
+import { } from "../actions";
+
+function mapStateToProps(state) {
+
+  return {
+    
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    
+  };
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(Basket);
