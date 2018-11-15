965a1577e1f58bd92a07e53a320837a8c182efae
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-11-15 14:49:39 +0000

    basket stylizing

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -7,48 +7,65 @@ export default class Basket extends React.Component {
   render() {
     let orderTotal = 0;
     const discount = 30;
-    const deliveryCharge = 2;
+    let deliveryCharge = 2;
+    let totalPrice = deliveryCharge;
     const Title = styled(Text)`
       font-size: 30px;
       text-align: center;
       padding: 10px;
     `;
+
     return (
       <View style={{}}>
         <Title style={{ fontSize: 30 }}>Your Order</Title>
-        <ScrollView>
+        <ScrollView style={{ paddingLeft: 5, paddingRight: 5 }}>
           {Object.values(this.props.currentOrder).map(dish => {
             let dishPrice = Number(dish.price);
             let dishFullPrice = dish.quantity * dishPrice;
             orderTotal += dishFullPrice;
-            if (orderTotal > discount) {
-              orderTotal = (orderTotal / 10) * 9;
-              return (
-                <Text key={dish.name}>
-                  {dish.name} x {dish.quantity} = £{dishFullPrice.toFixed(2)}
+            orderTotal >= discount
+              ? (deliveryCharge = 0)
+              : (deliveryCharge = 2);
+            totalPrice = orderTotal + deliveryCharge;
+            return (
+              <View
+                key={dish.name}
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between"
+                }}
+              >
+                <Text>
+                  {dish.quantity} x {dish.name}
                 </Text>
-              );
-            } else {
-              orderTotal = orderTotal + deliveryCharge;
-              let spendMore = discount - orderTotal;
-              return (
-                <View>
-                  <Text>
-                    {dish.name} x {dish.quantity} = £{dishFullPrice.toFixed(2)}
-                  </Text>
-                  <Text>
-                    Spend £{spendMore.toFixed(2)} more to get 10% off and free
-                    delivery!
-                  </Text>
-                </View>
-              );
-            }
+                <Text>£{dishFullPrice.toFixed(2)}</Text>
+              </View>
+            );
           })}
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text>Subtotal:</Text>
+            <Text>£{orderTotal.toFixed(2)}</Text>
+          </View>
+
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text>Delivery fee:</Text>
+            <Text>£{deliveryCharge.toFixed(2)}</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
+          >
+            <Text>Order total:</Text>
+            <Text>£{totalPrice.toFixed(2)}</Text>
+          </View>
         </ScrollView>
 
-        <Title style={{ fontSize: 30 }}>
-          Order total: £{orderTotal.toFixed(2)}
-        </Title>
         <Button
           raised
           icon={{ name: "add-shopping-cart" }}
@@ -57,7 +74,7 @@ export default class Basket extends React.Component {
             return this.props.emptyBasket();
           }}
           buttonStyle={{
-            backgroundColor: "red",
+            backgroundColor: "red"
           }}
         />
 
