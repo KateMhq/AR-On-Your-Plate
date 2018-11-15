405ee9f56781f1cd6f84e9e9f4bf577ae24a31c0
speirsluke <42845293+speirsluke@users.noreply.github.com>
2018-11-15 17:16:22 +0000

    Revert "hello"

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import {
   TouchableHighlight,
   Image,
 } from "react-native";
-import { AR, Camera, Permissions, Asset,AppLoading,  Font  } from "expo";
+import { AR, Camera, Permissions, Asset } from "expo";
 import ExpoTHREE, { THREE } from "expo-three";
 import ExpoGraphics from "expo-graphics";
 import GooglePoly from "./api/GooglePoly";
@@ -18,72 +18,27 @@ import rootReducer from "./src/reducers";
 import WrapperContainer from "./src/containers/WrapperContainer";
 import Router from "./Router";
 
-
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
 
 export default class App extends React.Component {
-
-  state = {
-    isLoadingComplete: false,
-  };
-
-
-  // async _loadAssetsAsync() {
-  //   const images = require('./assets/lobster.jpg')
-  //   const imageAssets = ([images]) => {
-  //     return images.map(image => {
-  //       if (typeof image == 'string'){
-  //         return Image.prefetch(image)
-  //       } else {
-  //         return Asset.fromModule(image).downloadAsync();
-  //       }
-  //     })
-  //   }
-  //
-  //   await Promise.all([...imageAssets]);
-  // }
-
-
   render() {
-    console.log('Render', this.state)
     console.disableYellowBox = true;
-
-    if (!this.state.isLoadingComplete){
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onFinish={this._handleFinishLoading}
-          onError={this._handleLoadingError}
-        />
-      );
-    } else {
-
-      return (
-        <Provider
-          store={createStore(rootReducer, applyMiddleware(thunkMiddleware))}
-          >
-            {/* <View style={styles.container}> */}
-            {/* <MenuItems menuItems={this.state.arrayWithMenuItems}/> */}
-            <Router />
-            {/* </View> */}
-          </Provider>
-        );
-    }
+    return (
+      <Provider
+        store={createStore(rootReducer, applyMiddleware(thunkMiddleware))}
+      >
+        {/* <View style={styles.container}> */}
+        {/* <MenuItems menuItems={this.state.arrayWithMenuItems}/> */}
+        <Router />
+        {/* </View> */}
+      </Provider>
+    );
   }
-
-  _loadResourcesAsync = async () => {
-    console.time('Loading assets')
-    return Asset.loadAsync([
-      require('./assets/lobster.jpg'),
-      require('./assets/splash.png')
-    ])
-  };
-
-  _handleLoadingError = error => {
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    console.timeEnd('Loading assets')
-    this.setState({ isLoadingComplete: true });
-  };
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -12,14 +12,20 @@ import {
 } from "react-native";
 import { Actions } from "react-native-router-flux";
 import styled from "styled-components/native";
-
-  import { AppLoading, Asset, Font } from 'expo';
 import { Icon } from "native-base";
 import { Button } from "react-native-elements";
 
 function Landing(props) {
   const BackgroundView = View;
-
+  // const BackgroundView = styled(View)`
+  //   height: 100%;
+  //   margin: 0 5px 5px 5px;
+  //   flex-direction: column;
+  //   border-radius: 15px;
+  //   align-items: center;
+  //   color: white;
+  //   justify-content: space-around;
+  // `;
   const TextHeader = styled(Text)`
     color: white;
     font-weight: 500;
@@ -34,7 +40,6 @@ function Landing(props) {
     font-weight: 400;
     text-align: center;
   `;
-
 
   const inputStyle = StyleSheet.create({
     input: {
@@ -55,7 +60,6 @@ function Landing(props) {
       style={{ width: "100%", height: "100%" }}
     >
       <BackgroundView>
-
         <TextHeader>AR On Your Plate</TextHeader>
         <TextDescription>Start your 3D order experience</TextDescription>
 
@@ -81,7 +85,6 @@ function Landing(props) {
           />
         </TypeSection>
 
-
         <TypeSection style={inputStyle.input}>
           <Icon style={{ padding: 10 }} type="FontAwesome" name="phone" />
           <TextInput
@@ -102,7 +105,6 @@ function Landing(props) {
           />
         </TypeSection>
 
-
         <Button
           icon={{
             name: "home",
@@ -111,12 +113,10 @@ function Landing(props) {
           }}
           backgroundColor="#ED6A5A"
           title="Enter"
-
           fontSize={23}
           color="white"
           fontWeight="550"
           borderRadius={15}
-
           onPress={() => {
             props.addUser(props.userName, props.userNumber), Actions.main();
           }}
