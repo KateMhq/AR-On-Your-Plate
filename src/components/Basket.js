d47032af42435eaceeb6f343a1534cadb4bdbb1b
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-11-15 15:42:34 +0000

    added discount

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import styled from "styled-components/native";
-import { Button, Icon } from "react-native-elements";
+import { Button, Icon, Divider } from "react-native-elements";
 import { Actions } from "react-native-router-flux";
 export default class Basket extends React.Component {
   render() {
@@ -9,6 +9,7 @@ export default class Basket extends React.Component {
     const discount = 30;
     let deliveryCharge = 2;
     let totalPrice = deliveryCharge;
+    let discountProcent = 0;
     const Title = styled(Text)`
       font-size: 30px;
       text-align: center;
@@ -26,7 +27,23 @@ export default class Basket extends React.Component {
             orderTotal >= discount
               ? (deliveryCharge = 0)
               : (deliveryCharge = 2);
-            totalPrice = orderTotal + deliveryCharge;
+            if (orderTotal <= 40) {
+              discountProcent = 0;
+            }
+            if (orderTotal > 40 && orderTotal <= 60) {
+              discountProcent = 5;
+            }
+            if (orderTotal > 60) {
+              discountProcent = 10;
+            }
+
+            if (discountProcent === 0) {
+              totalPrice = orderTotal + deliveryCharge;
+            } else {
+              totalPrice =
+                (orderTotal + deliveryCharge) * ((100 - discountProcent) / 100);
+            }
+
             return (
               <View
                 key={dish.name}
@@ -35,25 +52,40 @@ export default class Basket extends React.Component {
                   justifyContent: "space-between"
                 }}
               >
-                <Text>
+                <Text style={{ flex: 0.85 }}>
                   {dish.quantity} x {dish.name}
                 </Text>
-                <Text>£{dishFullPrice.toFixed(2)}</Text>
+                <Text style={{ flex: 0.25 }}>£{dishFullPrice.toFixed(2)}</Text>
               </View>
             );
           })}
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingTop: 10
+            }}
           >
-            <Text>Subtotal:</Text>
-            <Text>£{orderTotal.toFixed(2)}</Text>
+            <Text style={{ flex: 0.85 }}>Subtotal:</Text>
+            <Text style={{ flex: 0.25 }}>£{orderTotal.toFixed(2)}</Text>
           </View>
-
+          <Divider style={{ backgroundColor: "grey" }} />
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <Text>Delivery fee:</Text>
-            <Text>£{deliveryCharge.toFixed(2)}</Text>
+            <View style={{ flex: 0.85 }}>
+              <Text>Delivery fee:</Text>
+              <Text>(free delivery for orders over £30)</Text>
+            </View>
+            <Text style={{ flex: 0.25, alignSelf: "center" }}>
+              £{deliveryCharge.toFixed(2)}
+            </Text>
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ flex: 0.85 }}>Discount(5% > £40 or 10% > £60):</Text>
+            <Text style={{ flex: 0.25 }}>{discountProcent}%</Text>
           </View>
           <View
             style={{
@@ -61,8 +93,12 @@ export default class Basket extends React.Component {
               justifyContent: "space-between"
             }}
           >
-            <Text>Order total:</Text>
-            <Text>£{totalPrice.toFixed(2)}</Text>
+            <Text style={{ flex: 0.85, fontSize: 25 }}>Order total:</Text>
+            <Text
+              style={{ flex: 0.25, fontWeight: "bold", alignSelf: "center" }}
+            >
+              £{totalPrice.toFixed(2)}
+            </Text>
           </View>
         </ScrollView>
 
