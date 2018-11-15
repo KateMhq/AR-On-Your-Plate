49da9fbd2890ae52a95456254ae584d3c4a2449f
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-11-15 16:49:15 +0000

    adjusting in styling basket component ie. font size, font family

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -52,10 +52,12 @@ export default class Basket extends React.Component {
                   justifyContent: "space-between"
                 }}
               >
-                <Text style={{ flex: 0.85 }}>
+                <Text style={{ flex: 0.85, fontSize: 16 }}>
                   {dish.quantity} x {dish.name}
                 </Text>
-                <Text style={{ flex: 0.25 }}>£{dishFullPrice.toFixed(2)}</Text>
+                <Text style={{ flex: 0.25, fontSize: 16 }}>
+                  £{dishFullPrice.toFixed(2)}
+                </Text>
               </View>
             );
           })}
@@ -66,66 +68,83 @@ export default class Basket extends React.Component {
               paddingTop: 10
             }}
           >
-            <Text style={{ flex: 0.85 }}>Subtotal:</Text>
-            <Text style={{ flex: 0.25 }}>£{orderTotal.toFixed(2)}</Text>
+            <Text style={{ flex: 0.85, fontSize: 16 }}>Subtotal:</Text>
+            <Text style={{ flex: 0.25, fontSize: 16 }}>
+              £{orderTotal.toFixed(2)}
+            </Text>
           </View>
           <Divider style={{ backgroundColor: "grey" }} />
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
             <View style={{ flex: 0.85 }}>
-              <Text>Delivery fee:</Text>
-              <Text>(free delivery for orders over £30)</Text>
+              <Text style={{ fontSize: 16 }}>Delivery fee:</Text>
+              <Text style={{ fontSize: 12, fontFamily: "AvenirNext-Italic" }}>
+                (free delivery for orders over £30)
+              </Text>
             </View>
-            <Text style={{ flex: 0.25, alignSelf: "center" }}>
+            <Text style={{ flex: 0.25, alignSelf: "center", fontSize: 16 }}>
               £{deliveryCharge.toFixed(2)}
             </Text>
           </View>
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <Text style={{ flex: 0.85 }}>Discount(5% > £40 or 10% > £60):</Text>
-            <Text style={{ flex: 0.25 }}>{discountProcent}%</Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between"
-            }}
-          >
-            <Text style={{ flex: 0.85, fontSize: 25 }}>Order total:</Text>
-            <Text
-              style={{ flex: 0.25, fontWeight: "bold", alignSelf: "center" }}
-            >
-              £{totalPrice.toFixed(2)}
+            <View style={{ flex: 0.85, fontSize: 16 }}>
+              <Text style={{ fontSize: 16 }}>Discount:</Text>
+              <Text style={{ fontSize: 12, fontFamily: "AvenirNext-Italic" }}>
+                (5% > £40 or 10% > £60)
+              </Text>
+            </View>
+            <Text style={{ flex: 0.25, alignSelf: "center", fontSize: 16 }}>
+              {discountProcent}%
             </Text>
+          </View>
+          <View style={{ height: "100%" }} />
+          <View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between"
+              }}
+            >
+              <Text style={{ flex: 0.85, fontSize: 25 }}>Order total:</Text>
+              <Text
+                style={{ flex: 0.25, fontWeight: "bold", alignSelf: "center" }}
+              >
+                £{totalPrice.toFixed(2)}
+              </Text>
+            </View>
           </View>
         </ScrollView>
 
-        <Button
-          raised
-          icon={{ name: "add-shopping-cart" }}
-          title="Empty Basket"
-          onPress={() => {
-            return this.props.emptyBasket();
-          }}
-          buttonStyle={{
-            backgroundColor: "red"
-          }}
-        />
+        <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+          <Button
+            raised
+            icon={{ name: "add-shopping-cart" }}
+            title="Empty Basket"
+            onPress={() => {
+              return this.props.emptyBasket();
+            }}
+            buttonStyle={{
+              backgroundColor: "red"
+            }}
+          />
 
-        <Button
-          raised
-          icon={{ name: "add-shopping-cart" }}
-          title="Complete Order"
-          onPress={() => {
-            return (
-              this.props.postOrder(this.props.currentOrder),
-              this.props.emptyBasket(),
-              Actions.main()
-            );
-          }}
-        />
+          <Button
+            style={{ width: "100%" }}
+            raised
+            icon={{ name: "add-shopping-cart" }}
+            title="Complete Order"
+            onPress={() => {
+              return (
+                this.props.postOrder(this.props.currentOrder),
+                this.props.emptyBasket(),
+                Actions.main()
+              );
+            }}
+          />
+        </View>
       </View>
     );
   }
